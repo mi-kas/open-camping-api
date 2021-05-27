d55f17a45b84b64d03775093dd92fce445ace7f8
mi-kas <mi.kaserer@gmail.com>
2021-05-27 22:58:07 +0200

    Controller updated

--- api/src/controllers/camping.ts
+++ api/src/controllers/camping.ts
@@ -1,12 +1,22 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { searchCamping, CampingSearch } from "../services/camping";
 
-export const findNearby = async (req: Request, res: Response) => {
+export const findNearby = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const offset = Number(req.query?.offset ?? 0);
   const limit = Number(req.query?.limit ?? 20);
   let searchParams: CampingSearch = { offset, limit };
 
   if (req.query?.lat && req.query?.lng) {
+    if (!req.query?.radius) {
+      return next({
+        status: 400,
+        message: "request.query should have required property 'radius'"
+      });
+    }
     searchParams = {
       ...searchParams,
       location: {
