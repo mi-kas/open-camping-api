0f704c5ec8e3498e98ee2730db07f3f6144694ee
mi-kas <mi.kaserer@gmail.com>
2021-06-07 18:07:38 +0200

    Country import improved

--- importer/scripts/mongo_import.sh
+++ importer/scripts/mongo_import.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+# Load environment variables from .env file
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
 echo "Start import of seed data into mongodb"
-mongoimport --collection=countries --file=scripts/countries.json --jsonArray --drop --uri=$1
+mongoimport --collection=countries --file=scripts/countries.json --jsonArray --drop --uri=$DB_URI
 echo "Import of seed data done"
 
